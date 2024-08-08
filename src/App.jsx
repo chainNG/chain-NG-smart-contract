95adf114246d688010e69d804957b7cea286b498
Abang Obed <66479041+d3vobed@users.noreply.github.com>
2024-08-08 09:55:12 +0000

    chore: more fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 
 import ProtectedRoute from "./Reusables/ProtectedRoute"
-import { Home ,Login,Signup,Analytics} from "./pages/index"
+import { Home,
+        Login,
+        Signup, 
+        Analytics } from "./pages/index"
 import {Routes,Route} from 'react-router-dom'
 function App() {
 
 
   return (
     
-     <div className="flex    items-center justify-center w-full h-screen ">
+     <div>
       <Routes>
         <Route path='/' element={<ProtectedRoute>
           <Home/>
