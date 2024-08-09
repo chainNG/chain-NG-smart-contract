b825e9df6d7928021be289f3684b9b1c9442fa90
Abang Obed <66479041+d3vobed@users.noreply.github.com>
2024-08-09 23:59:26 +0000

    update: renders request

---
+++ src/components/Buttons/Button.jsx
@@ -0,0 +1,45 @@
+import { LoadingAnimation } from "../../assets/RenderedAssets";
+import { Link } from "react-router-dom";
+
+const Button = ({
+  value,
+  textColor = "text-white",
+  btnColor = "bg-secondary",
+  type,
+  click,
+  disabled = false,
+  isLoading,
+  icon,
+  url = "/",
+}) => {
+  return (
+    <>
+      {type == null ? (
+        <Link
+          to={`${url}`}
+          className={`rounded-md ${btnColor} ${textColor} py-4 px-12 text-20 transition-all duration-500 ease-in-out hover:${btnColor}/40`}
+        >
+          {value}
+        </Link>
+      ) : (
+        <button
+          type={type}
+          disabled={disabled}
+          className={` ${
+            isLoading ? "cursor-default" : "cursor-pointer"
+          } h-[45px] w-[333px] ${btnColor} ${textColor} rounded-[50px] border-[1px] mt-[50px]  border-gray-700 flex justify-center items-center bold`}
+          onClick={isLoading ? () => {} : click}
+        >
+          {icon === null ? null : (
+            <img src={icon} alt="icon" className="mr-3" />
+          )}
+          {isLoading ? <LoadingAnimation /> : null}
+          {value}
+        </button>
+      )}
+    </>
+  );
+};
+
+export default Button;
+
---
+++ src/components/CustomTexts/TextFade.jsx
@@ -0,0 +1,25 @@
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+const TextFade = ({ animation, children, style, isSpan = false }) => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
+  return (
+    <>
+      {isSpan ? (
+        <span data-aos={animation} className={`${style}`}>
+          {children}
+        </span>
+      ) : (
+        <div data-aos={animation} className={`${style}`}>
+          {children}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default TextFade;
+
