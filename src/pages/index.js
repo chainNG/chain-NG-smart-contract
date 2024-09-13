4e011bede7e73cd572a715e6010c5f5c3f5e133a
Abang Obed <66479041+d3vobed@users.noreply.github.com>
2024-09-13 09:02:22 +0000

    feat: update linking

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,8 @@
-export  {default as  LandingPage} from './Landing/LandingPage'
 export  {default as  Home} from './Home'
 export  {default as  Signup} from './Signup'
 export {default as Login} from './Login'
 export {default as Analytics} from './Analytics'
-export {default as ContactPage} from './Contact/ContactPage'
-export {default as AboutPage} from './About/AboutPage'
+
   /**
     export {default as Analytics} from './Analytics'
     export {default as Analytics} from './Analytics'
