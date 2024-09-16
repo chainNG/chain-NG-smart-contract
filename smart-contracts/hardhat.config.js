d00dd4b7760a1c772d7972f36e29081164c25a20
cyber <adebayoridwan57@gmail.com>
2024-09-16 04:13:56 -0400

    contract commit

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -1,6 +1,15 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-    solidity: "0.8.19",
+  solidity: "0.8.0",  // Ensure this matches your Solidity version
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/5942bc7bb9274ba4a927c440f9697a7f`,
+      accounts: [``]  // Add your private key, preferably via an environment variable
+    }
+  },
+  etherscan: {
+    apiKey: "YOUR_POLYGONSCAN_API_KEY"  // Optional, for verifying the contract on Polygonscan
   }
+};
